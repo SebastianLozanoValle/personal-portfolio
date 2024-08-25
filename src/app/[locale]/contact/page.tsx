70751518ee544909d2bf02bb23c966ef640a5b9d
SebastianLozanoValle <cesarloco2003@hotmail.com>
2024-08-25 02:47:33 -0500

    el resposive perfectooo de la nav jaja, si quiero de nuevo la nav de pc completamente centrada debo volver a la version anterio, sin drama porque dejar anotada la version vieja se veria mal

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -17,14 +17,14 @@ export default async function  Contact({ params: { locale }} : { params: { local
                 {/* <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-orange-400 to-purple-500 bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
                     Retro Grid
                 </span> */}
-                <div className="container mx-auto p-20 flex flex-wrap justify-center items-center">
+                <div className="lg:container lg:mx-auto py-20 lg:p-20 flex flex-wrap justify-center items-center w-full">
                     <div className="w-full flex flex-col items-center p-4 justify-center">
                         <h2 className="text-center text-[2.25rem] lg:text-[4.5rem] font-extrabold bg-gradient-to-t from-purple-500 to-orange-400 bg-clip-text transition-all duration-500 text-transparent">
                             {t("title")}
                         </h2>
                         {/* <p>(this component isnt already traduced and styled, it will soon. Thank you for your understanding.)Se que a veces llenar un formulario puede ser extenuante por eso he decidido solicitar la menor cantidad de informacion posibe para dar el debido seguimiento al proceso que desida emprender conmigo, agradezco el tiempo que dedicara en el y tambien lo he realizado con cmucho esmero para que vea misa avilidades en validacion de formulario, ux y manejo de los datos con el fin de poder obtener datos relevantes para sus bases de datos</p> */}
                     </div>
-                    <div className="w-full p-4">
+                    <div className="p-4 w-full">
                         <div className="p-4 bg-slate-100 rounded-lg dark:bg-slate-800">
                             <ContactForm />
                         </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,51 +2,51 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useTranslation } from "react-i18next";
-import { CgMenuRight } from "react-icons/cg";
+import { CgMenuRight, CgClose } from "react-icons/cg";
 import LanguageChanger from '@/components/LanguageChanger'
 import { useState } from "react";
 
 export const NavBar = () => {
     const pathname = usePathname();
-    const [isMobileOpen, setIsMobileOpen] = useState()
+    const [isMobileOpen, setIsMobileOpen] = useState(false);
 
     const { t } = useTranslation();
 
     return (
-        <nav className={`z-[100] top-0 left-0 p-4 lg:px-40 flex justify-between items-center h-[80px] ${pathname.includes('contact') ? 'fixed w-full text-pseudoblack dark:text-background bg-background dark:bg-pseudoblack' : 'sticky bg-pseudoblack text-background dark:shadow-orange-400 dark:shadow'}`}>
-            <Link href={'/'} className="text-3xl font-extrabold bg-gradient-to-r from-purple-500 to-orange-400 bg-clip-text transition-all duration-500 hover:text-transparent">
-                Sebastian Dev
-            </Link>
+        <nav className={`z-[100] top-0 left-0 p-4 lg:pr-16 flex justify-between items-center h-[80px] ${pathname.includes('contact') ? 'fixed w-full text-pseudoblack dark:text-background bg-background dark:bg-pseudoblack' : 'sticky bg-pseudoblack text-background dark:shadow-orange-400 dark:shadow'}`}>
+            <div className="lg:flex-[1] flex lg:justify-around">
+                <Link href={'/'} className="text-3xl font-extrabold bg-gradient-to-r from-purple-500 to-orange-400 bg-clip-text transition-all duration-500 hover:text-transparent">
+                    Sebastian Dev
+                </Link>
+            </div>
             
-            <button className="text-5xl relative lg:absolute lg:hidden">
-                <CgMenuRight />
+            <button onClick={() => {setIsMobileOpen(!isMobileOpen)}} className="text-5xl lg:hidden">
+                {isMobileOpen ? <CgClose /> : <CgMenuRight />}
             </button>
-            
-            <ul className="hidden lg:flex absolute lg:relative gap-10 items-center justify-center text-lg font-bold">
-                <li className="group relative">
-                    <span className="absolute z-10 bottom-1 -left-1 h-2 w-0 group-hover:w-full bg-orange-400 dark:bg-purple-500 transition-all duration-500 rounded-full"/>
-                    <Link className="relative z-20" href={'/'}>{t('nav.home')}</Link>
-                </li>
-                <li className="group relative">
-                    <span className="absolute z-10 bottom-1 -left-1 h-2 w-0 group-hover:w-full bg-orange-400 dark:bg-purple-500 transition-all duration-500 rounded-full"/>
-                    <Link className="relative z-20" href={'/'}>{pathname === '/' || pathname === '/en' || pathname.includes('contact') ? t('nav.services') : t('nav.skills')}</Link>
-                </li>
-                <li className="group relative">
-                    <span className="absolute z-10 bottom-1 -left-1 h-2 w-0 group-hover:w-full bg-orange-400 dark:bg-purple-500 transition-all duration-500 rounded-full"/>
-                    <Link className="relative z-20" href={'/'}>{t('nav.projects')}</Link>
-                </li>
-                {/* <li className="group relative">
-                    <span className="absolute z-10 bottom-1 -left-1 h-2 w-0 group-hover:w-full bg-orange-400 dark:bg-purple-500 transition-all duration-500 rounded-full"/>
-                    <Link className="relative z-20" href={'/'}>{t('nav.language')}</Link>
-                </li> */}
-                <LanguageChanger languages={t('nav.language')} />
-            </ul>
 
-            <Link href={'/contact'} className="hidden lg:flex absolute lg:relative font-bold text-lg px-4 xl:px-8 py-2 rounded-full bg-gradient-to-r from-purple-500 to-orange-400 text-pseudoblack hover:text-background transition-all duration-500 text-center">
-                {t('nav.cta')}
-            </Link>
+            <div className={`fixed top-[80px] right-0 bg-background/30 dark:bg-pseudoblack/30 backdrop-blur-sm transition-all ease-in-out duration-500 ${isMobileOpen ? 'w-full h-[calc(100vh-80px)] rounded-bl-none' : 'w-0 h-0 rounded-bl-3xl'}`}></div>
 
-            {/* class=" bg-gradient-to-r from-purple-500 to-orange-400 bg-clip-text transition-all duration-500 text-transparent" */}
+            <div className={`fixed lg:flex-[2] lg:justify-around lg:relative top-[80px] lg:top-0 lg:right-auto flex flex-col lg:flex-row gap-8 lg:gap-10 transition-all justify-center items-center ease-in-out duration-1000 ${isMobileOpen ? 'h-[calc(100vh-80px)] w-full visible right-0' : 'h-0 w-0 lg:h-auto lg:w-auto lg:visible -right-[400px]'}`}>
+                <ul className="flex flex-col lg:flex-row gap-4 lg:gap-10 items-center justify-center text-base lg:text-lg font-bold">
+                    <li className="group relative">
+                        <span className="absolute z-10 bottom-1 -left-1 h-2 w-0 group-hover:w-full bg-orange-400 dark:bg-purple-500 transition-all duration-500 rounded-full"/>
+                        <Link onClick={() => {setIsMobileOpen(false)}} className="relative z-20" href={'/'}>{t('nav.home')}</Link>
+                    </li>
+                    <li className="group relative">
+                        <span className="absolute z-10 bottom-1 -left-1 h-2 w-0 group-hover:w-full bg-orange-400 dark:bg-purple-500 transition-all duration-500 rounded-full"/>
+                        <Link onClick={() => {setIsMobileOpen(false)}} className="relative z-20" href={'/'}>{pathname === '/' || pathname === '/en' || pathname.includes('contact') ? t('nav.services') : t('nav.skills')}</Link>
+                    </li>
+                    <li className="group relative">
+                        <span className="absolute z-10 bottom-1 -left-1 h-2 w-0 group-hover:w-full bg-orange-400 dark:bg-purple-500 transition-all duration-500 rounded-full"/>
+                        <Link onClick={() => {setIsMobileOpen(false)}} className="relative z-20" href={'/'}>{t('nav.projects')}</Link>
+                    </li>
+                    <LanguageChanger languages={t('nav.language')} />
+                </ul>
+
+                <Link onClick={() => {setIsMobileOpen(false)}} href={'/contact'} className="font-bold text-lg px-4 xl:px-8 py-2 rounded-full bg-gradient-to-r from-purple-500 to-orange-400 text-pseudoblack hover:text-background transition-all duration-500 text-center lg:block">
+                    {t('nav.cta')}
+                </Link>
+            </div>
         </nav>
     )
 }
