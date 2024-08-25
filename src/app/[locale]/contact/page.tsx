0b9fc860b11998faa9da1fbaf7e662ec5b1674a7
SebastianLozanoValle <cesarloco2003@hotmail.com>
2024-08-25 01:41:30 -0500

    se me habia olvidado tambien cambiar el titulo de la pagina de contacto, el resposive

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -19,7 +19,7 @@ export default async function  Contact({ params: { locale }} : { params: { local
                 </span> */}
                 <div className="container mx-auto p-20 flex flex-wrap justify-center items-center">
                     <div className="w-full flex flex-col items-center p-4 justify-center">
-                        <h2 className="text-center text-[4.5rem] font-extrabold bg-gradient-to-t from-purple-500 to-orange-400 bg-clip-text transition-all duration-500 text-transparent">
+                        <h2 className="text-center text-[2.25rem] lg:text-[4.5rem] font-extrabold bg-gradient-to-t from-purple-500 to-orange-400 bg-clip-text transition-all duration-500 text-transparent">
                             {t("title")}
                         </h2>
                         {/* <p>(this component isnt already traduced and styled, it will soon. Thank you for your understanding.)Se que a veces llenar un formulario puede ser extenuante por eso he decidido solicitar la menor cantidad de informacion posibe para dar el debido seguimiento al proceso que desida emprender conmigo, agradezco el tiempo que dedicara en el y tambien lo he realizado con cmucho esmero para que vea misa avilidades en validacion de formulario, ux y manejo de los datos con el fin de poder obtener datos relevantes para sus bases de datos</p> */}
